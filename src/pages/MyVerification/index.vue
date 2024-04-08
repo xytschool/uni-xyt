569edc56813046fa0249837c289557bfb844f590
litong <tong4_li@trtjk.com>
2024-04-09 00:14:38 +0800

    feat: 人脸识别

--- src/pages/MyVerification/index.vue
+++ src/pages/MyVerification/index.vue
@@ -1,43 +1,41 @@
 <template>
   <view class="page-content">
-    <view class="contentV">
-      <camera
-        v-if="isAuthCamera"
-        device-position="front"
-        class="camera"
-        flash="off"
-        resolution="high"
-      />
-      <image v-if="tempImg" mode="widthFix" :src="tempImg" />
-      <view v-show="!tempImg && tipsText" class="tipV">{{ tipsText }}</view>
-    </view>
-    <view class="footerV">
-      <view style="width: 100%;">
-        <view v-if="!tempImg" style="width: 100%;">
-          <view class="verify ">
-            请进行人脸识别验证身份
-          </view>
-          <view class="verify">
-            并确保本人操作
-          </view>
-
-          <view class="take-photo-bgV">
-            <button
-              type="primary"
-              class="start-verification"
-              @click="handleTakePhotoClick"
-            >
-              开始录入
-            </button>
-          </view>
+    <view class="containerV">
+      <view class="headerV">
+        <view class="top-tips1">
+          <view>请将正对手机，头部匹配摄像区域</view>
         </view>
+      </view>
 
-        <view class="confirmV" v-else>
-          <view class="btn-cancel btn" @click="handleCancelClick">
-            取消
+      <view class="contentV">
+        <view class="mark"></view>
+        <image v-if="tempImg" mode="widthFix" :src="tempImg" />
+        <camera
+          v-if="isAuthCamera"
+          device-position="front"
+          class="camera"
+          flash="off"
+          resolution="high"
+        />
+        <view v-show="!tempImg && tipsText" class="tipV">{{ tipsText }}</view>
+      </view>
+
+      <view class="footerV">
+        <view style="width: 100%;">
+          <view v-if="!tempImg" style="width: 100%;">
+            <view class="take-photo-bgV">
+              <!--拍照-->
+              <view class="btn-take-photo" @click="handleTakePhotoClick" />
+              <!-- 切换镜头 -->
+            </view>
           </view>
-          <view class="btn-ok btn" @click="handleOkClick">
-            确定
+          <view class="confirmV" v-else>
+            <view class="btn-cancel" @click="handleCancelClick">
+              取消
+            </view>
+            <view class="btn-ok" @click="handleOkClick">
+              确定
+            </view>
           </view>
         </view>
       </view>
@@ -46,6 +44,9 @@
 </template>
 
 <script>
+import { getAuthToken } from '../../utils/utils.js'
+import { baseUrl } from '../../../src/config'
+
 export default {
   name: 'index',
   components: {},
@@ -53,19 +54,22 @@ export default {
     return {
       tipsText: '', // 错误文案提示
       tempImg: '', // 本地图片路径
+
       cameraEngine: null, // 相机引擎
+
       isAuthCamera: true // 是否拥有相机权限
     }
   },
 
-  onLoad() {
+  onLoad(options) {
     this.initData()
   },
-  mounted() {},
+
   methods: {
     // 初始化相机引擎
     initData() {
       // #ifdef MP-WEIXIN
+
       // 1、初始化人脸识别
       wx.initFaceDetect()
       // 2、创建 camera 上下文 CameraContext 对象
@@ -76,7 +80,7 @@ export default {
           return
         }
         // 4、人脸识别，使用前需要通过 wx.initFaceDetect 进行一次初始化，推荐使用相机接口返回的帧数据
-        wx.createVKSession({
+        wx.faceDetect({
           frameBuffer: frame.data,
           width: frame.width,
           height: frame.height,
@@ -136,7 +140,6 @@ export default {
 
       uni.getSetting({
         success: (res) => {
-          console.log(res.authSetting['scope.camera'], 'scope')
           if (!res.authSetting['scope.camera']) {
             this.isAuthCamera = false
             uni.openSetting({
@@ -153,27 +156,45 @@ export default {
         quality: 'high',
         success: ({ tempImagePath }) => {
           this.tempImg = tempImagePath
-          this.isAuthCamera = false
+          console.log('=======tempImg:', this.tempImg)
         }
       })
-      //
     },
+
     // 点击确定上传
     handleOkClick() {
-      //  这里的 this.tempImg 是经过人脸检测后  拍照拿到的路径
-      this.upLoadOne(this.tempImg)
-      uni.navigateBack({
-        delta: 1
+      console.log(this.tempImg, 'this.tempImg')
+      wx.uploadFile({
+        filePath: this.tempImg,
+        name: 'file',
+        header: {
+          Authorization: getAuthToken()
+        },
+        url: `${baseUrl}/14/api/v1/user/face`,
+        success: (res) => {
+          if (JSON.parse(res.data).code == 'success') {
+            uni.showToast({
+              title: '上传成功',
+              duration: 2000
+            })
+            setTimeout(() => {
+              uni.navigateBack()
+            }, 2000)
+          }
+        },
+        fail: (err) => {
+          uni.showToast({
+            title: `上传失败,${err}`,
+            duration: 2000
+          })
+          console.log('失败...', err)
+        }
       })
     },
-    upLoadOne(imgPath) {
-      //  然后这里imgPath 传过来的是 要上传的临时本地图片的路径
-      //  具体上传方法根据自己的请求方式   请求自己的接口
-    },
+
     // 点击 - 取消上传
     handleCancelClick() {
       this.tempImg = ''
-      this.isAuthCamera = true
     }
   }
 }
@@ -183,106 +204,118 @@ export default {
 .page-content {
   width: 100%;
   height: 100%;
-  .contentV {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 661rpx;
-    margin-top: 30rpx;
-    .tipV {
-      margin-top: 20rpx;
-      line-height: 90rpx;
-      padding-left: 24rpx;
-      padding-right: 24rpx;
-      max-width: calc(100vw - 50rpx * 2);
-      text-align: center;
-      font-size: 30rpx;
-      background: #000000;
-      opacity: 0.75;
-      color: #ffffff;
-      border-radius: 16rpx;
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      z-index: 5;
-    }
-    .camera {
-      width: 448rpx;
-      height: 448rpx;
-      border-radius: 50% !important;
-    }
 
-    image {
-      margin: 30rpx auto;
-    }
-  }
-
-  .footerV {
+  .containerV {
     width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+    height: 100%;
 
-    .take-photo-bgV {
-      width: 100%;
+    .headerV {
+      .top-tips1 {
+        margin-top: 60rpx;
+        color: #1c1c1c;
+        font-size: 36rpx;
+        text-align: center;
+      }
+    }
+
+    .contentV {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 661rpx;
       margin-top: 30rpx;
+
+      .tipV {
+        bottom: 30rpx;
+        position: absolute;
+        line-height: 90rpx;
+        padding-left: 24rpx;
+        padding-right: 24rpx;
+        max-width: calc(100vw - 50rpx * 2);
+        text-align: center;
+        font-size: 30rpx;
+        background: #000000;
+        opacity: 0.75;
+        color: #ffffff;
+        border-radius: 16rpx;
+
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        z-index: 5;
+      }
+
+      .camera {
+        width: 100%;
+        height: 100%;
+      }
+
+      .mark {
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 2;
+        width: 750rpx;
+        height: 100%;
+        background: url('@/static/face/view_face_background.png') no-repeat
+          center bottom;
+        background-size: 750rpx 661rpx;
+      }
+
+      image {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index: 3;
+      }
+    }
+
+    .footerV {
+      width: 100%;
       display: flex;
       flex-direction: row;
       align-items: center;
       justify-content: center;
-      .start-verification {
-        width: 640rpx;
-        height: 100rpx;
-        background: #71b459;
-        border-radius: 82rpx 82rpx 82rpx 82rpx;
-        font-family: PingFang SC, PingFang SC;
-        font-weight: 600;
-        font-size: 32rpx;
-        color: #ffffff;
-        line-height: 100rpx;
-        text-align: left;
-        font-style: normal;
-        text-transform: none;
-        text-align: center;
-        position: absolute;
-        bottom: 158rpx;
-        left: 50%;
-        margin-left: -320rpx;
+
+      .take-photo-bgV {
+        width: 100%;
+        margin-top: 30rpx;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+
+        .btn-take-photo {
+          // 由于左边没有按钮，所以左边要便宜更大，以便是拍照按钮居中
+          margin: 0rpx 80rpx 0rpx 80rpx;
+          width: 196rpx;
+          height: 196rpx;
+          background: url('https://pro-file-qn.ztjy61.com/1003020211103145058685NNR9vlTm.png')
+            no-repeat;
+          background-size: 100% auto;
+        }
       }
-    }
-    .confirmV {
-      margin: 200rpx 100rpx 0rpx 100rpx;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: space-between;
-      .btn {
-        width: 188rpx;
-        height: 60rpx;
-        background: #649c4a;
-        border-radius: 76rpx 76rpx 76rpx 76rpx;
-        font-family: PingFang SC, PingFang SC;
-        font-weight: 400;
-        font-size: 28rpx;
-        color: #ffffff;
-        line-height: 60rpx;
-        text-align: left;
-        font-style: normal;
-        text-align: center;
+
+      .confirmV {
+        margin: 200rpx 100rpx 0rpx 100rpx;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+
+        .btn-cancel {
+          font-size: 32rpx;
+          color: #1c1c1c;
+        }
+
+        .btn-ok {
+          font-size: 32rpx;
+          color: #00aaff;
+        }
       }
     }
   }
-}
-.verify {
-  font-family: PingFang SC, PingFang SC;
-  font-weight: 400;
-  font-size: 32rpx;
-  color: #000000;
-  line-height: 52rpx;
-  text-align: center;
-  font-style: normal;
 }
 </style>
