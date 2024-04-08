573d9ac0e3a3b49cee37a50efdc197bb6fd2ce68
litong <tong4_li@trtjk.com>
2024-04-09 00:08:50 +0800

    feat: 解决request获取不到com_id

--- src/config.js
+++ src/config.js
@@ -2,5 +2,7 @@
 //export const baseUrl = 'http://m.xytschool.com/dev'
 
 export const baseUrl = 'https://mall.lo.mytool.zone/{$com_id}/api/v1'
+export const baseUrlLogo = 'https://mall.lo.mytool.zone/14/api/v1'
+
 
 
