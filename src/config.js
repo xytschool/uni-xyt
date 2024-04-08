7b655604d359311eac9503501fa7a81c811c47e1
litong <tong4_li@trtjk.com>
2024-04-08 19:59:08 +0800

    feat: 请求接口改为https

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 //export const baseUrl = 'http://m.xytschool.com/v1'
 //export const baseUrl = 'http://m.xytschool.com/dev'
 
-export const baseUrl = 'http://mall.lo.mytool.zone/{$com_id}/api/v1'
+export const baseUrl = 'https://mall.lo.mytool.zone/{$com_id}/api/v1'
 
 
