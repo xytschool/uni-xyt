3bff7fef84ad214996282ee8cc7f007f02a3b7a5
litong <tong4_li@trtjk.com>
2024-04-24 20:45:42 +0800

    feat: 修改请求地址

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 //export const baseUrl = 'http://m.xytschool.com/v1'
 //export const baseUrl = 'http://m.xytschool.com/dev'
 
-export const baseUrl = 'https://mall.lo.mytool.zone/{$com_id}/api/v1'
-export const baseUrlLogo = 'https://mall.lo.mytool.zone/14/api/v1'
+export const baseUrl = 'https://mall.nanwanhu.com.cn/{$com_id}/api/v1'
+export const baseUrlLogo = 'https://mall.nanwanhu.com.cn/14/api/v1'
 
 
 
