432fc730bd30edc708f6152f356bc98ecb9b79e2
litong <tong4_li@trtjk.com>
2024-05-27 09:06:07 +0800

    feat: 更改图片地址

--- src/pages/introduction/index.vue
+++ src/pages/introduction/index.vue
@@ -3,7 +3,7 @@
     <view class="title">南湾湖景区</view>
     <image
       class="headDiagram"
-      src="https://data.xytschool.com/2024/05/26/665318bbd6d47.jpg"
+      src="https://data.xytschool.com/2024/05/27/6653d95141f77.jpg"
     ></image>
     <view class="title">
       景区简介
@@ -19,7 +19,9 @@
     </view>
     <view class="introduce">
       <view class="introduce-item">
-        <image src="https://data.xytschool.com/2024/05/26/665318b4a696c.jpg"></image>
+        <image
+          src="https://data.xytschool.com/2024/05/26/665318b4a696c.jpg"
+        ></image>
         <view class="title">茶岛</view>
         <view class="content">
           茶岛位于南湾湖中，是南 湾湖最大的岛屿之一。岛 上茶园茂盛，是品尝正宗
@@ -27,7 +29,9 @@
         </view>
       </view>
       <view class="introduce-item">
-        <image src="https://data.xytschool.com/2024/05/26/665318b51188d.jpg"></image>
+        <image
+          src="https://data.xytschool.com/2024/05/26/665318b51188d.jpg"
+        ></image>
         <view class="title">花链岛</view>
         <view class="content">
           花链岛是南湾湖中的一处 花鲜养殖基地，岛上花鲜 品种繁多，是观赏和钓鱼
@@ -35,7 +39,9 @@
         </view>
       </view>
       <view class="introduce-item">
-        <image src="https://data.xytschool.com/2024/05/26/665318b54c6be.jpg"></image>
+        <image
+          src="https://data.xytschool.com/2024/05/26/665318b54c6be.jpg"
+        ></image>
         <view class="title">鸟岛</view>
         <view class="content">
           鸟岛是南湾湖的保护区， 是许多鸟类的栖息地。岛 上可以观赏到各种珍稀鸟
@@ -43,7 +49,9 @@
         </view>
       </view>
       <view class="introduce-item">
-        <image src="https://data.xytschool.com/2024/05/26/665318b738e96.jpg"></image>
+        <image
+          src="https://data.xytschool.com/2024/05/26/665318b738e96.jpg"
+        ></image>
         <view class="title">猴岛</view>
         <view class="content">
           猴岛是南湾湖中的一座小 岛，岛上有大量的猴群栖 息，游客可以近距离观察
@@ -54,7 +62,7 @@
     <view class="title">文化遗址</view>
     <image
       class="headDiagram"
-      src="https://data.xytschool.com/2024/05/26/665318b738e96.jpg"
+      src="https://data.xytschool.com/2024/05/27/6653d959d12a4.jpg"
     ></image>
     <view class="title">梁王磊遗址</view>
     <view class="content">
@@ -63,7 +71,7 @@
     <view class="title">旅游设施</view>
     <image
       class="headDiagram"
-      src="https://data.xytschool.com/2024/05/26/665318bbd6d47.jpg"
+      src="https://data.xytschool.com/2024/05/27/6653d95497117.jpg"
     ></image>
     <view class="title">南湾湖大坝</view>
     <view class="content">
@@ -81,12 +89,11 @@
 
     <image
       class="headDiagram"
-      src="https://data.xytschool.com/2024/05/26/665318bbd6d47.jpg"
+      src="https://data.xytschool.com/2024/05/27/6653d95c63220.jpg"
     ></image>
     <view class="title">南湾水上游乐园</view>
     <view class="content">
-      南湾水上游乐园是南湾湖边的一处娱乐设施，提供丰富
-的水上游玩项目和设施。
+      南湾水上游乐园是南湾湖边的一处娱乐设施，提供丰富 的水上游玩项目和设施。
     </view>
   </view>
 </template>
