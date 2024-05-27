61dce509ab923d31f015ed86f244cce522e1b25f
litong <tong4_li@trtjk.com>
2024-05-27 09:06:24 +0800

    feat: 线上路径更改

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 //export const baseUrl = 'http://m.xytschool.com/v1'
 //export const baseUrl = 'http://m.xytschool.com/dev'
 
-export const baseUrl = 'https://mall.nanwanhu.com.cn/{$com_id}/api/v1'
-export const baseUrlLogo = 'https://mall.nanwanhu.com.cn/14/api/v1'
+export const baseUrl = 'https://mall.mytool.zone/{$com_id}/api/v1'
+export const baseUrlLogo = 'https://mall.mytool.zone/14/api/v1'
 
+// export const baseUrl = 'https://mall.nanwanhu.com.cn/{$com_id}/api/v1'
+// export const baseUrlLogo = 'https://mall.nanwanhu.com.cn/14/api/v1'
 
 
