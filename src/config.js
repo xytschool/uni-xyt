1bcec7e59929d35a32f42625a55924a77c6d5450
litong <tong4_li@trtjk.com>
2024-06-11 09:37:46 +0800

    feat: 修改接口地址

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 //export const baseUrl = 'http://m.xytschool.com/v1'
 //export const baseUrl = 'http://m.xytschool.com/dev'
 
-export const baseUrl = 'https://mall.mytool.zone/{$com_id}/api/v1'
-export const baseUrlLogo = 'https://mall.mytool.zone/14/api/v1'
+// export const baseUrl = 'https://mall.mytool.zone/{$com_id}/api/v1'
+// export const baseUrlLogo = 'https://mall.mytool.zone/14/api/v1'
 //本地
 // export const baseUrl = 'https://gomall.loc.mytool.zone/{$com_id}/api/v1'
 // export const baseUrlLogo = 'https://gomall.loc.mytool.zone/14/api/v1'
 
 //测试
-// export const baseUrl = 'https://gomall.test.mytool.zone/{$com_id}/api/v1'
-// export const baseUrlLogo = 'https://gomall.test.mytool.zone/14/api/v1'
+export const baseUrl = 'https://gomall.test.mytool.zone/{$com_id}/api/v1'
+export const baseUrlLogo = 'https://gomall.test.mytool.zone/14/api/v1'
 
 // export const baseUrl = 'https://mall.nanwanhu.com.cn/{$com_id}/api/v1'
 // export const baseUrlLogo = 'https://mall.nanwanhu.com.cn/14/api/v1'
