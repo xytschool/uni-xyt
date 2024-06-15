569f1b0c0cc5558d3953ecb825bac615f89ce29e
litong <tong4_li@trtjk.com>
2024-06-15 23:09:10 +0800

    feat: 更改关于我们图片

--- src/pages/introduction/index.vue
+++ src/pages/introduction/index.vue
@@ -20,7 +20,7 @@
     <view class="introduce">
       <view class="introduce-item">
         <image
-          src="https://data.xytschool.com/2024/05/26/665318b4a696c.jpg"
+          src="https://data.xytschool.com/2024/06/15/666da85b97aab.jpeg"
         ></image>
         <view class="title">茶岛</view>
         <view class="content">
@@ -30,17 +30,16 @@
       </view>
       <view class="introduce-item">
         <image
-          src="https://data.xytschool.com/2024/05/26/665318b51188d.jpg"
+          src="https://data.xytschool.com/2024/06/15/666dad57904e4.jpg"
         ></image>
-        <view class="title">花链岛</view>
+        <view class="title">樱花园</view>
         <view class="content">
-          花链岛是南湾湖中的一处 花鲜养殖基地，岛上花鲜 品种繁多，是观赏和钓鱼
-          的好地方。
+          樱花园就位于茶岛上。春风醉茶都，樱漫南湾湖。每年的的四月份左右，正值樱花盛开的季节。
         </view>
       </view>
       <view class="introduce-item">
         <image
-          src="https://data.xytschool.com/2024/05/26/665318b54c6be.jpg"
+          src="https://data.xytschool.com/2024/06/15/666dad9b7928e.jpg"
         ></image>
         <view class="title">鸟岛</view>
         <view class="content">
@@ -50,7 +49,7 @@
       </view>
       <view class="introduce-item">
         <image
-          src="https://data.xytschool.com/2024/05/26/665318b738e96.jpg"
+          src="https://data.xytschool.com/2024/06/15/666da85b76263.jpg"
         ></image>
         <view class="title">猴岛</view>
         <view class="content">
@@ -59,7 +58,7 @@
         </view>
       </view>
     </view>
-    <view class="title">文化遗址</view>
+    <!-- <view class="title">文化遗址</view>
     <image
       class="headDiagram"
       src="https://data.xytschool.com/2024/05/27/6653d959d12a4.jpg"
@@ -67,34 +66,34 @@
     <view class="title">梁王磊遗址</view>
     <view class="content">
       梁王垒是南湾湖附近的一处古代军事遗址，是历史文化 的重要见证。
-    </view>
+    </view> -->
     <view class="title">旅游设施</view>
     <image
       class="headDiagram"
-      src="https://data.xytschool.com/2024/05/27/6653d95497117.jpg"
+      src="https://data.xytschool.com/2024/06/15/666daab64e9ae.jpg"
     ></image>
     <view class="title">南湾湖大坝</view>
     <view class="content">
-      南湾湖大坝是南湾湖的重要水利工程，也是游客观赏湖 景和休闲娱乐的好地方。
+      南湾湖大坝长816.5米，坝顶宽8米，坝高38.3米，最大拦蓄水量为16.3亿立方米，大坝正面镶砌有毛泽东所题的“一定要把淮河修好”八个大字，坝顶、坝坡安装投光灯、高压钠灯共369盏，在东西坝头分设2盏高杆灯，并在高杆灯上设4台激光射灯。白天漫步坝顶，大坝如一条长龙，横贯东西，气势恢宏，放眼远眺，但觉碧波荡漾，颇有临江濒海之感。而夜幕下的大坝，灯火通明、金碧辉煌，灯光映照在湖面上波光粼粼，呈现出如梦如幻般景象，令人流连忘返。
     </view>
 
     <image
       class="headDiagram"
-      src="https://data.xytschool.com/2024/05/26/665318bbd6d47.jpg"
+      src="https://data.xytschool.com/2024/06/15/666dab625a02e.jpg"
     ></image>
     <view class="title">贤隐寺</view>
-    <view class="content">
-      贤隐寺是南湾湖附近的一座古老寺庙，是信仰和文化交 流的场所。
+    <view class="content" style="padding-bottom: 50rpx;">
+      贤隐寺为古时“中原五大寺”之一，与“嵩山少林寺、洛阳白马寺、开封相国寺、南阳玄观庙”齐名。
     </view>
 
-    <image
+    <!-- <image
       class="headDiagram"
       src="https://data.xytschool.com/2024/05/27/6653d95c63220.jpg"
     ></image>
     <view class="title">南湾水上游乐园</view>
     <view class="content" style="padding-bottom: 50rpx;">
       南湾水上游乐园是南湾湖边的一处娱乐设施，提供丰富 的水上游玩项目和设施。
-    </view>
+    </view> -->
   </view>
 </template>
 
@@ -137,6 +136,8 @@
   margin-right: 26rpx;
   image {
     width: 100%;
+    height: 480rpx;
+    object-fit: contain;
   }
 }
 .introduce-item:nth-child(2n) {
